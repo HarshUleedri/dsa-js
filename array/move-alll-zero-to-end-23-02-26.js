1959678c022ccc922f7175d171fa561d99c4ac10
HarshUleedri <ulidari2@gmail.com>
2026-02-24 08:22:05 +0000

    move all zero to end

---
+++ array/move-alll-zero-to-end-23-02-26.js
@@ -0,0 +1,40 @@
+const arr = [0, 1, 0, 3, 0, 34, 53, 0, 543, 0, 12];
+
+function moveAllZeroToEnd(arr) {
+  if (arr.length === 0) return null;
+
+  let allZeroAtEnd = [];
+  let zero = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      allZeroAtEnd.push(arr[i]);
+    } else {
+      zero.push(0);
+    }
+  }
+
+  return [...allZeroAtEnd, ...zero];
+}
+
+const result = moveAllZeroToEnd(arr);
+console.log(result);
+
+// optimize solution
+//  time complexity O(n) space compexity O(1)
+const arr1 = [0, 0, 3, 5, 0, 7, 0, 12, 13];
+
+function moveZero(arr) {
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
+    }
+  }
+
+  return arr;
+}
+
+console.log(moveZero(arr1));
