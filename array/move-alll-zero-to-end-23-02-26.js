cab3350682b86cbb8530e8edfaa64e9867f6f7b3
HarshUleedri <ulidari2@gmail.com>
2026-02-25 16:46:26 +0000

    shifting negative number to left

--- array/move-alll-zero-to-end-23-02-26.js
+++ array/move-alll-zero-to-end-23-02-26.js
@@ -1,4 +1,4 @@
-const arr = [0, 1, 0, 3, 0, 34, 53, 0, 543, 0, 12];
+// const arr = [0, 1, 0, 3, 0, 34, 53, 0, 543, 0, 12];s
 
 function moveAllZeroToEnd(arr) {
   if (arr.length === 0) return null;
@@ -17,8 +17,8 @@ function moveAllZeroToEnd(arr) {
   return [...allZeroAtEnd, ...zero];
 }
 
-const result = moveAllZeroToEnd(arr);
-console.log(result);
+// const result = moveAllZeroToEnd(arr)
+// console.log(result);
 
 // optimize solution
 //  time complexity O(n) space compexity O(1)
@@ -37,4 +37,20 @@ function moveZero(arr) {
   return arr;
 }
 
-console.log(moveZero(arr1));
+// console.log(moveZero(arr1));
+
+const arr2 = [1, -2, 3, -4, 5, -6, -7];
+
+function moveNegativeToLeft(arr) {
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
+    }
+  }
+  return arr;
+}
+
+console.log(moveNegativeToLeft(arr2));
