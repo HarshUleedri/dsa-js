7c99415009e3a0fec19186f7f38a7385df7a1f0d
HarshUleedri <ulidari2@gmail.com>
2026-07-03 18:12:10 +0000

    question 2 solved of binary search

--- array/algorithms/binary-search/solving_binary_search_questions.js
+++ array/algorithms/binary-search/solving_binary_search_questions.js
@@ -1,5 +1,5 @@
-arr = [2, 4, 6, 8, 10, 12];
-target = 8;
+const arr = [2, 4, 6, 8, 10, 12];
+const target = 8;
 
 function findTheIndex(arr, target) {
   for (let i = 0; i < arr.length; i++) {
@@ -11,3 +11,17 @@ function findTheIndex(arr, target) {
 }
 
 console.log(findTheIndex(arr, target));
+
+const arr2 = [1, 2, 2, 2, 3, 4];
+const target2 = 2;
+
+function findFirstAccurence(arr, target) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(findFirstAccurence(arr2, target2));
