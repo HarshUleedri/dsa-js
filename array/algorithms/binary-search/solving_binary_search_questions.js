e93770cb94b79585845cba2066626d3a36cddb3f
HarshUleedri <ulidari2@gmail.com>
2026-07-04 02:41:44 +0000

    binary search question solved

--- array/algorithms/binary-search/solving_binary_search_questions.js
+++ array/algorithms/binary-search/solving_binary_search_questions.js
@@ -51,3 +51,39 @@ function findAccurence(arr, target) {
 }
 
 console.log(findAccurence(arr3, target3));
+const arr4 = [1, 3, 5, 6];
+const target4 = 2;
+function getTheIndexOfTargetToMaintainOrder(arr, target) {
+  for (i = 1; i < arr.length; i++) {
+    if (target > arr[i - 1] && target < arr[i + 1]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(getTheIndexOfTargetToMaintainOrder(arr4, target4), "question 5");
+const arr5 = [3, 8, 12, 17, 25, 31, 42];
+const target5 = 25;
+function findTarget(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if ((arr[middle] === target)) {
+      return middle;
+    }
+
+    if (target > arr[middle]) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(findTarget(arr5, target5));
