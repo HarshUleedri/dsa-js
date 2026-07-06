f46af9abf26dc5ad10a87f6d97575b9b6f96d384
HarshUleedri <ulidari2@gmail.com>
2026-07-06 11:57:20 +0000

    binary search question solved

--- array/algorithms/binary-search/solving_binary_search_questions.js
+++ array/algorithms/binary-search/solving_binary_search_questions.js
@@ -186,3 +186,24 @@ function floorElement(arr, target) {
 }
 
 console.log(floorElement([2, 4, 6, 8, 10], 7));
+
+function ceilingElement(arr, target) {
+  let first = 0;
+  let last = arr.length - 1;
+  let ans = -1;
+  while (first <= last) {
+    const mid = Math.floor((first + last) / 2);
+
+    if (arr[mid] >= target) {
+      ans = arr[mid];
+      last = mid - 1;
+    } else if (arr[mid] < target) {
+      first = mid + 1;
+    } else {
+      lasst = mid - 1;
+    }
+  }
+  return ans;
+}
+
+console.log(ceilingElement([2, 4, 6, 8, 10], 7));
