bb957fdd2d3e2ba772c1e64d7a7bf6b36d4100bf
HarshUleedri <ulidari2@gmail.com>
2026-07-07 18:36:14 +0000

    binary search question solved

--- array/algorithms/binary-search/solving_binary_search_questions.js
+++ array/algorithms/binary-search/solving_binary_search_questions.js
@@ -224,3 +224,29 @@ function findPeakElement(arr) {
 }
 
 console.log(findPeakElement([1, 3, 7, 6, 4]));
+
+function searchInRotatedArr(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  // let ans = -1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return arr[mid];
+    if (arr[left] <= arr[mid]) {
+      if (target >= arr[left] && target < arr[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > arr[mid] && target <= arr[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+console.log(searchInRotatedArr([40, 50, 60, 70, 10, 20, 30], 20));
