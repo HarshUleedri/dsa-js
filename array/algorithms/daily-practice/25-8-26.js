3906cc3b02cf8b8a0870ca6e209082320b8f7816
HarshUleedri <ulidari2@gmail.com>
2026-08-25 04:54:41 +0000

    flatten array question solved

--- array/algorithms/daily-practice/25-8-26.js
+++ array/algorithms/daily-practice/25-8-26.js
@@ -24,6 +24,40 @@ while (left < right) {
   right--;
 }
 
-console.log(arr2)
+console.log(arr2);
 
+//question 2 flatten nested arr
+
+const arr3 = [1, [2, [3, 4], 5]];
+
+function flatten(arr) {
+  let flattenArr = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (typeof arr[i] === "object") {
+      flattenArr = [...flattenArr, ...flatten(arr[i])];
+    } else {
+      flattenArr.push(arr[i]);
+    }
+  }
+
+  return flattenArr;
+}
+
+console.log(flatten(arr3), "flatten array");
+
+//optimize version
+
+function optiFlatten(arr) {
+  const result = [];
+  for (let el of arr) {
+    if (Array.isArray(el)) {
+      result.push(...optiFlatten(el));
+    } else {
+      result.push(el);
+    }
+  }
+  return result;
+}
+
+console.log(optiFlatten(arr3), "opti flatten array");
 
