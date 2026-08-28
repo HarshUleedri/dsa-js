cc1ce9cf343ad81fe8864fd7bbb4279f14eeff00
HarshUleedri <ulidari2@gmail.com>
2026-08-28 12:21:12 +0000

    question solved

--- array/algorithms/daily-practice/25-8-26.js
+++ array/algorithms/daily-practice/25-8-26.js
@@ -459,3 +459,20 @@ function createUnion(arr1, arr2) {
 }
 
 console.log(createUnion([1, 2, 3, 4, 5], [4, 5, 6, 7, 8]));
+
+function sortArry(arr) {
+  let temp;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++)
+      if (arr[j] >= arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+  }
+
+  return arr;
+}
+
+console.log(sortArry([2, 1, 3, 5, 6, 4]));
