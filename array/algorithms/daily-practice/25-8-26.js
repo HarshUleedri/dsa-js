dd60f15eac53838cfcee2acda06b4949e240e0f7
HarshUleedri <ulidari2@gmail.com>
2026-08-30 18:18:22 +0000

    question solved

--- array/algorithms/daily-practice/25-8-26.js
+++ array/algorithms/daily-practice/25-8-26.js
@@ -526,3 +526,18 @@ function findFirstNonRepeating(string) {
 }
 
 console.log(findFirstNonRepeating("Harsh"));
+
+function sortthenumber(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] >= arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortthenumber([3, 4, 1, 5, 7, 9, 2]));
